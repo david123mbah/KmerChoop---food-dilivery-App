1893fa38e91497f9ed22b27e5af57760050411b6
david123mbah <davidmbah1234@gmail.com>
2024-08-04 15:51:41 +0100

    added the food price and fix image

--- lib/pages/food_page.dart
+++ lib/pages/food_page.dart
@@ -2,13 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:musicapp/models/food.dart';
 
 class Foodpage extends StatelessWidget {
+  final Food food;
 
-   final Food food;
-
-
-  const Foodpage({super.key ,
-  required this.food,
-  
+  const Foodpage({
+    super.key,
+    required this.food,
   });
 
   @override
@@ -17,46 +15,65 @@ class Foodpage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Image.asset(food.imagePath),
-        
-        
-            // Container 
+            Image.asset(food.imagePath , width: 500, height: 400, fit: BoxFit.cover,),
             
-            // food name 
-            Text(
-              food.name,
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-        
-        
-            // food description
+
+            // Container
+
+            // food name
             Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                food.description,
-                style: Theme.of(context).textTheme.bodyMedium,
+              padding: const EdgeInsets.all(25.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    food.name,
+                    style: const TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  // food price 
+                   Text(
+                      food.price.toString(),
+                      style: TextStyle(
+                      fontSize: 22,
+                      color: Theme.of(context).colorScheme.primary),
+                    ),
+
+                    const SizedBox(height: 10,),
+                  // food description
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      food.description,
+                      style: TextStyle(
+                      fontSize: 16,
+                      color: Theme.of(context).colorScheme.primary),
+                    ),
+                  ),
+              
+                  // addons
+                  Material(
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: food.availableAddons.length,
+                      itemBuilder: ((context, index) {
+                        // get individual addon
+                        Addon addon = food.availableAddons[index];
+                        return CheckboxListTile(
+                          title: Text(addon.name),
+                          value: false,
+                          onChanged: (value) {},
+                        );
+                      }),
+                    ),
+                  ),
+                ],
               ),
             ),
-        
-        
-            // addons
-            Material(
-          
-              child: ListView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: food.availableAddons.length,
-                itemBuilder: ((context, index) {
-                  // get individual addon 
-                  Addon addon = food.availableAddons[index];
-                  return CheckboxListTile(
-                    title: Text(addon.name),
-                    value: false, 
-                    onChanged: (value) {},
-                    );
-                }),
-              ),
-            )
           ],
         ),
       ),
